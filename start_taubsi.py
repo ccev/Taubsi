fd585d098c4fa42ae5eaa11858ad62e7acc4baed
Malte <42342921+ccev@users.noreply.github.com>
2021-10-03 13:15:55 +0200

    this didn't commit

--- start_taubsi.py
+++ start_taubsi.py
@@ -3,6 +3,7 @@ from taubsi.core.config_classes import Cog
 from taubsi.cogs.raids.raid_commands import RaidCommand
 from taubsi.cogs.playerstats.playerstats_commands import (StatContext, StatsCommand, LinkCommand,
                                                           UnlinkCommand, LeaderboardCommand)
+from taubsi.cogs.setup.setup_commands import (NameCommand, LevelCommand, LevelUpCommand, TeamCommand)
 
 bot.load_cogs()
 
@@ -11,6 +12,9 @@ for server in bot.servers:
         bot.add_application_command(command=RaidCommand, guild_id=server.id)
     if Cog.PLAYERSTATS in bot.config.COGS:
         for command in [StatContext, StatsCommand, LinkCommand, UnlinkCommand, LeaderboardCommand]:
+            bot.add_application_command(command=command, guild_id=server.id)
+    if Cog.SETUP in bot.config.COGS:
+        for command in [NameCommand, LevelCommand, LevelUpCommand, TeamCommand]:
             bot.add_application_command(command=command, guild_id=server.id)
 
 bot.run(bot.config.BOT_TOKEN)
