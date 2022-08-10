ca285b528b9b3568b13799efb83739f7685bbaeb
Malte <42342921+ccev@users.noreply.github.com>
2022-08-10 21:46:48 +0200

    nicer logging

--- taubsi/core/logging.py
+++ taubsi/core/logging.py
@@ -1,4 +1,5 @@
 import sys
+from loguru import logger
 import logging
 import argparse
 
@@ -12,6 +13,51 @@ if args.debug:
 else:
     log_level = logging.INFO
 
-logging.basicConfig(format="[%(asctime)s] [%(name)s] [%(levelname)-1.1s]  %(message)s", level=log_level,
-                    datefmt="%H:%M:%S", stream=sys.stdout)
-log = logging.getLogger("Taubsi")
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+console_format = (
+    "<cyan>{time:HH:mm:ss.SS}</cyan>",
+    "<level>{level: >1.1}</level>",
+    "<cyan>{name: <22.22}</cyan>",
+    "<level>{message}</level>"
+)
+console_format = " | ".join(console_format)
+
+logger.remove()
+
+logger.add(
+    sink=sys.stdout,
+    format=console_format,
+    colorize=True,
+    level=log_level,
+    enqueue=True
+)
+logger.add(
+    sink=sys.stderr,
+    format=console_format,
+    colorize=True,
+    level=logging.ERROR,
+    backtrace=True,
+    enqueue=True
+)
+
+logger.configure(extra={"identifier": "unknown"})
+logging.basicConfig(handlers=[InterceptHandler()], level=0)
+
+log = logger
